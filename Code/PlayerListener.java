220f3ae79439929f99343110b26dc0e5e39c82da
Ibrahim <ibinshikh@hotmail.com>
2017-07-12 04:02:43 +0300

    Added method javadoc

--- Code/PlayerListener.java
+++ Code/PlayerListener.java
@@ -5,9 +5,16 @@
  */
 
 /**
- *
+ * An interface that can be implemented by any class that is interested
+ * on player actions.
  * @author Ibrahim
  */
 public interface PlayerListener {
+    /**
+     * This method is called when the player is playing his turn. 
+     * @param source the player who did initiate the event.
+     * @param rowIndex the row index on the game board.
+     * @param columnIndex the column index on the game board.
+     */
     public void play(Player source, int rowIndex, int columnIndex);
 }
